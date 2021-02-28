278d06bb7205fde30d5c4a21ee02d4a35dba704e
Yong Sun <wssy213@sina.cn>
2021-02-28 09:42:21 +0800

    remove temporary database files on exit.
    Signed-off-by: Yong Sun <wssy213@sina.cn>

--- logdb/rocksdb_test.cpp
+++ logdb/rocksdb_test.cpp
@@ -3,16 +3,22 @@
 // 		g++ -std=c++11 -Wall rocksdb_test.cpp \
 // 		-I /path/to/rocksdb-rootdirectory/include/ \
 // 		-L/path/to/rocksdb-rootdirectory/ \
-// 		-lpthread -ldl -lz -lbz2 -llz4 -lzstd -o rocksdb_test
+// 		-lrocksdb -lpthread -ldl -lz -lbz2 -llz4 -lzstd \
+// 		-o rocksdb_test
 //
 #include <iostream>
 #include <string>
+
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <fts.h>
 #include <time.h>
 #include <stdlib.h>
 #include <getopt.h>
 #include "rocksdb/db.h"
 extern char * optarg;
+extern int rmdir_r(const char *dir);
 
 using std::cout;
 using std::endl;
@@ -22,7 +28,7 @@ int main(int argc, char * argv[])
 	//default value for args
 	int count = 100;
 	long unit = 1024 * 1024; // 1MB
-	std::string dbpath("rocksdb.db");
+	std::string dbpath("/tmp/rocksdb.db");
 	struct timespec starttsp;
 	struct timespec endtsp;
 	struct timespec elapsed;
@@ -89,8 +95,76 @@ int main(int argc, char * argv[])
 	std::printf("rough throughput: %.3lf MB per second\n",
 				((double)unit * count / (1024*1024)) / totaltime);
 	
+	if(rmdir_r(dbpath.c_str()) != 0)
+		cout << "temporary database files '"
+			<< dbpath << "' cannot remove\n";
 	delete pdata;
 	//close db
 	delete db;
 	return 0;
+}
+
+//delete directory recursively.
+//helper function browsed from internel library
+int rmdir_r(const char *dir)
+{
+	int ret = 0;
+	FTS *ftsp = NULL;
+	FTSENT *curr;
+
+	char *files[] = {(char *)dir, NULL};
+
+	// FTS_NOCHDIR  - Avoid changing cwd, which could cause unexpected behavior
+	//                in multithreaded programs
+	// FTS_PHYSICAL - Don't follow symlinks. Prevents deletion of files outside
+	//                of the specified directory
+	// FTS_XDEV     - Don't cross filesystem boundaries
+	ftsp = fts_open(files, FTS_NOCHDIR | FTS_PHYSICAL | FTS_XDEV, NULL);
+	if (!ftsp) {
+		fprintf(stderr, "%s: fts_open failed: %s\n", dir, strerror(errno));
+		ret = -1;
+		goto finish;
+	}
+
+	while ((curr = fts_read(ftsp))) {
+		switch (curr->fts_info) {
+			case FTS_NS:
+			case FTS_DNR:
+			case FTS_ERR:
+				fprintf(stderr, "%s: fts_read error: %s\n",
+					curr->fts_accpath, strerror(curr->fts_errno));
+				break;
+
+			case FTS_DC:
+			case FTS_DOT:
+			case FTS_NSOK:
+				// Not reached unless FTS_LOGICAL, FTS_SEEDOT, or FTS_NOSTAT were
+				// passed to fts_open()
+				break;
+
+			case FTS_D:
+				// Do nothing. Need depth-first search, so directories are deleted
+				// in FTS_DP
+				break;
+
+			case FTS_DP:
+			case FTS_F:
+			case FTS_SL:
+			case FTS_SLNONE:
+			case FTS_DEFAULT:
+				if (remove(curr->fts_accpath) < 0) {
+					fprintf(stderr, "%s: Failed to remove: %s\n",
+						curr->fts_path, strerror(curr->fts_errno));
+					ret = -1;
+				}
+				break;
+		}
+	}
+
+finish:
+	if (ftsp) {
+		fts_close(ftsp);
+	}
+
+	return ret;
 }
